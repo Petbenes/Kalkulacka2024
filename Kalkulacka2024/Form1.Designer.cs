6f3edf62cd5ad9b27f67d318f0ef48a1a52fe439
Petbenes <129893069+Petbenes@users.noreply.github.com>
2024-03-13 13:22:56 +0100

    Add files via upload

---
+++ Kalkulacka2024/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Kalkulacka2024
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDisplej = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtDisplej
+            // 
+            this.txtDisplej.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtDisplej.Location = new System.Drawing.Point(12, 12);
+            this.txtDisplej.Name = "txtDisplej";
+            this.txtDisplej.Size = new System.Drawing.Size(394, 29);
+            this.txtDisplej.TabIndex = 0;
+            this.txtDisplej.Text = "0";
+            this.txtDisplej.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 477);
+            this.Controls.Add(this.txtDisplej);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Kalkulačka";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDisplej;
+    }
+}
+
